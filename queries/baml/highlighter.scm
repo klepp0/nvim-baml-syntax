3de03d5da7969dd1c9e0b1eef9436de7b65a1bb0
klepp0 <jonas.kleppich@sap.com>
2024-12-08 00:55:42 +0100

    feat: add queries

--- queries/baml/highlighter.scm
+++ queries/baml/highlighter.scm
@@ -1,1 +1,30 @@
+;; queries/baml/highlighter.scm
 
+;; Highlight comments
+(comment) @comment
+(doc_comment) @comment.documentation
+
+;; Highlight keywords
+(function_declaration) @keyword
+(class_declaration) @keyword
+(enum_declaration) @keyword
+(test_declaration) @keyword
+(generator_declaration) @keyword
+(retry_policy_declaration) @keyword
+(template_string_declaration) @keyword
+
+;; Highlight identifiers
+(identifier) @identifier
+
+;; Highlight strings
+(string) @string
+
+;; Highlight annotations
+(single_annotation) @annotation
+(double_annotation) @annotation
+
+;; Highlight types
+(type) @type
+
+;; Highlight numbers
+(number) @number
