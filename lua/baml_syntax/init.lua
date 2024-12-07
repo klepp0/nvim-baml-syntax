05564810f64ff483839b6b4de2ea02798b85b487
klepp0 <jonas.kleppich@sap.com>
2024-12-07 23:22:56 +0100

    build: fix branch

--- lua/baml_syntax/init.lua
+++ lua/baml_syntax/init.lua
@@ -1,9 +1,13 @@
 -- lua/baml_syntax/init.lua
-
-require 'nvim-treesitter.parsers'.get_parser_configs().baml = {
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.baml = {
     install_info = {
         url = "https://github.com/klepp0/nvim-baml-syntax",
         files = { "parser/src/parser.c" },
+        -- optional entries:
+        branch = "main",
+        generate_requires_npm = false,          -- if stand-alone parser without npm dependencies
+        requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
     },
     filetype = "baml",
 }
