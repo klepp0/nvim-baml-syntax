e6fea64ec88b90ccf86a7e7afad50abc5344ad52
klepp0 <jonas.kleppich@sap.com>
2024-12-08 21:28:21 +0100

    feat: update higlighter

--- queries/baml/highlighter.scm
+++ queries/baml/highlighter.scm
@@ -1,10 +1,18 @@
 ;; queries/baml/highlighter.scm
 
-;; Highlight comments
-(comment) @comment
-(doc_comment) @comment.documentation
+;; =============================================================================
+;; BAML Syntax Highlighting Definitions
+;; =============================================================================
 
-;; Highlight keywords
+;; ---------------------------
+;; Comments
+;; ---------------------------
+(comment) @comment
+(docstring) @comment
+
+;; ---------------------------
+;; Keywords (Declarations)
+;; ---------------------------
 (function_declaration) @keyword
 (class_declaration) @keyword
 (enum_declaration) @keyword
@@ -13,18 +21,62 @@
 (retry_policy_declaration) @keyword
 (template_string_declaration) @keyword
 
-;; Highlight identifiers
-(identifier) @identifier
+;; ---------------------------
+;; Function and Type Names
+;; ---------------------------
+(function_declaration name: (identifier)) @function
 
-;; Highlight strings
-(string) @string
+(class_declaration name: (identifier)) @type
+(enum_declaration name: (identifier)) @type
+(test_declaration name: (identifier)) @type
+(generator_declaration name: (identifier)) @type
+(retry_policy_declaration name: (identifier)) @type
+(template_string_declaration name: (identifier)) @type
 
-;; Highlight annotations
-(single_annotation) @annotation
-(double_annotation) @annotation
+;; ---------------------------
+;; Parameters and Types
+;; ---------------------------
+(parameter) @parameter
 
-;; Highlight types
 (type) @type
 
-;; Highlight numbers
+;; ---------------------------
+;; Strings and Numbers
+;; ---------------------------
+(string) @string
+(block_string) @string
+
 (number) @number
+
+;; ---------------------------
+;; Operators
+;; ---------------------------
+(_arrow) @operator
+
+;; ---------------------------
+;; Environment Variables and Identifiers
+;; ---------------------------
+(environment_variable name: (identifier)) @variable.builtin
+
+(identifier) @identifier
+
+;; ---------------------------
+;; Expressions and Structures
+;; ---------------------------
+(function_body) @function
+(union_type) @type
+(primitive_type) @type
+(array_type) @type
+(map_literal) @structure
+(map_entry) @structure
+
+;; ---------------------------
+;; Additional Highlighting
+;; ---------------------------
+(map_entry name: (identifier)) @property
+(block) @block
+
+;; =============================================================================
+;; End of BAML Syntax Highlighting Definitions
+;; =============================================================================
+
